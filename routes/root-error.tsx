7fe8e27331b7985aacc9b3d8df44cadabe2f81d1
solomonakp <solomonakp@gmail.com>
2025-02-06 18:04:21 +0100

    fix(blog): updated root error

--- routes/root-error.tsx
+++ routes/root-error.tsx
@@ -7,11 +7,15 @@ import { Box, Button, Title } from '@mantine/core';
 export const RootErrorPage = () => {
   const router = useRouter();
   return (
-    <Page isError className="home-page--error">
-      <Title order={1}>Something Went Wrong</Title>
-      <Box mt="xl">
-        <Button onClick={() => router.push('/')}>Try again</Button>
-      </Box>
-    </Page>
+    <html lang="en">
+      <body>
+        <Page isError className="home-page--error">
+          <Title order={1}>Something Went Wrong</Title>
+          <Box mt="xl">
+            <Button onClick={() => router.push('/')}>Try again</Button>
+          </Box>
+        </Page>
+      </body>
+    </html>
   );
 };
